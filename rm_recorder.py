adb69fa90dc4c8cbd426a35cf91fa61a3a2fea51
whlphlgcn <705843711@qq.com>
2024-05-30 14:40:42 +0800

    v1.0.0.6
    1，新增视角关闭不录制功能

--- rm_recorder.py
+++ rm_recorder.py
@@ -11,34 +11,39 @@ def make_recorder_info(live_url, live_level):
 
 def start_all_recorder(recorder_info):
     for k in recorder_info:
-        rm_ffmpeg.run_ffmpeg_single_cmd(recorder_info, k)
+        if recorder_info[k]["level"] != 'disable':
+            rm_ffmpeg.run_ffmpeg_single_cmd(recorder_info, k)
 
 
 def close_all_recorder(recorder_info):
     for k in recorder_info:
-        rm_ffmpeg.close_ffmpeg_single_cmd(recorder_info, k)
+        if recorder_info[k]["level"] != 'disable':
+            rm_ffmpeg.close_ffmpeg_single_cmd(recorder_info, k)
 
 
 def check_process_state(recorder_info):
     for k in recorder_info:
-        if recorder_info[k]["process"] is not None:
-            if recorder_info[k]["process"].poll() is not None:
-                recorder_info[k]["process"] = None
+        if recorder_info[k]["level"] != 'disable':
+            if recorder_info[k]["process"] is not None:
+                if recorder_info[k]["process"].poll() is not None:
+                    recorder_info[k]["process"] = None
 
 
 def get_close_process_num(recorder_info):
     num = 0
     for k in recorder_info:
-        if recorder_info[k]["process"] is None:
-            num += 1
+        if recorder_info[k]["level"] != 'disable':
+            if recorder_info[k]["process"] is None:
+                num += 1
     return num
 
 
 def get_open_process_num(recorder_info):
     num = 0
     for k in recorder_info:
-        if recorder_info[k]["process"] is not None:
-            num += 1
+        if recorder_info[k]["level"] != 'disable':
+            if recorder_info[k]["process"] is not None:
+                num += 1
     return num
 
 
